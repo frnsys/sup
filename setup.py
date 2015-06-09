5fcf2dd94ccaf504d248ba72f869a2e6721fa325
Francis Tseng <f+accounts@frnsys.com>
2015-06-08 20:43:05 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sup',
+    version='0.0.1',
+    description='misc utils',
+    url='https://github.com/ftzeng/sup',
+    author='Francis Tseng (@frnsys)',
+    license='MIT',
+
+    zip_safe=True,
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
